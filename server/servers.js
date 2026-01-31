a039f55c6b9dc772b1ed34fe30c68a35d5361dc0
Eva Langerova <langerova_eva@yahoo.com>
2026-01-31 15:28:35 +0100

    setup firebase admin

--- server/servers.js
+++ server/servers.js
@@ -1,6 +1,10 @@
 // Create express and socket.io servers
 const express = require("express");
 const app = express();
+const admin = require("firebase-admin");
+
+require("dotenv").config({ path: "./config/.env" });
+
 const expressServer = app.listen(9000, () => {
   console.log("Server running on port 9000");
 });
@@ -8,6 +12,13 @@ const expressServer = app.listen(9000, () => {
 const { Server } = require("socket.io");
 const io = new Server(expressServer, {
   cors: ["http://localhost:5173"],
+});
+
+// initialize firebase admin
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = { app, io };
